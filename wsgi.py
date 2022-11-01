4ba8ac2bbdfbda3e574f662818221e58305d333f
Sandro Boelsz <sandro.boelsz@hva.nl>
2022-11-01 16:32:25 +0100

    wsgi aanpassing

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,4 @@
-from flask import Flask
-
-def init_app():
-    app = Flask(__name__)
-
-    with app.app_context():
-        from visualisation.algemene_moteriek_dashboard import init_dashboard
-        app = init_dashboard(app) 
-        return app
-
-app = init_app()
+from app import app
 
 if __name__ == "__main__":
     app.run()
