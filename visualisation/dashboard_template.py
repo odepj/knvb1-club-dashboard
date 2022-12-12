89b78e8071de08b6c75d55162d30ef9a0334078b
Bart Sprenkeling <bart.sprenkeling@hva.nl>
2022-12-12 22:56:00 +0100

    commit to end the day, updated line-graph and added basic filtering

--- visualisation/dashboard_template.py
+++ visualisation/dashboard_template.py
@@ -9,20 +9,29 @@ from flask import session
 from visualisation import algemene_motoriek_chart
 import plotly.graph_objs as go
 from visualisation.dashboard_template_functions import calculate_mean_result_by_date, add_figure_rangeslider, \
-    filter_bloc_tests
+    filter_bloc_tests, get_colormap, rename_column, filter_measurements, get_measurement_columns
 
 # this list contains the names of all the unique bvo's in the database
 BVO_LIST = request_bvo()
-META_COLUMNS = ['id', 'bvo_naam', 'seizoen', 'Testdatum', 'reeks_naam', 'team_naam', 'display_name', 'speler_id',
-                'geboortedatum', 'Staande_lengte']
 
 
 def save_filter_to_session(key: str, value: str) -> None:
     session[key] = value
 
 
-def retrieve_filter_from_session(key: str) -> str:
+def retrieve_filter_from_session(key: str):
     return session.get(key)
+
+
+def fix_labels(df: pd.DataFrame) -> dict:
+    df_ticktext = df[['seizoen', 'reeks_naam']].drop_duplicates()
+    ticktext = [f"""{row.seizoen.removeprefix('20')}, {row.reeks_naam}""" for row in df_ticktext.itertuples()]
+    tickvals = df['Testdatum'].unique()
+    return dict(
+        tickmode='array',
+        tickvals=tickvals,
+        ticktext=ticktext  # tuple van (23/24 - na/voorjaar)
+    )
 
 
 # This method is used by the app.py to initialize the Dash dashboard in Flask
@@ -33,7 +42,6 @@ def init_dashboard_template(server):
         routes_pathname_prefix="/dashboard/dash/dashboard_template/",
         external_stylesheets=[dbc.themes.BOOTSTRAP]
     )
-
 
     # All the layout items such as the dashboard's charts itself are put in this 'layout' variable below
     dash_app.layout = dbc.Container(
@@ -49,12 +57,13 @@ def init_dashboard_template(server):
                 dbc.Col(
                     width=2,
                     children=[
-                        html.Div(id="bloc_test_selection"),
                         html.Div(id="filter_selection"),
+                        html.Div(id="bloc_test_selection"),
+                        html.Div(id="measurement_selection"),
                         html.Div(id="statistics_selection"),
                         html.Div(id="benchmark_selection"),
                     ],
-                style={"height": "10rem"}),
+                    style={"height": "10rem"}),
                 # Graphs
                 dbc.Col(
                     width=10,
@@ -111,13 +120,12 @@ def init_callbacks(dash_app):
 
         # add the default bloc-test selection
         values = ["Evenwichtsbalk", "Zijwaarts springen",
-                        "Zijwaarts verplaatsen", "Hand-oog coördinatie"]
+                  "Zijwaarts verplaatsen", "Hand-oog coördinatie"]
 
         session_bloc = retrieve_filter_from_session("bloc_test_selection")
         if session_bloc is None:
             session_bloc = values
             save_filter_to_session("bloc_test_selection", values)
-
 
         return dbc.Card([
             dbc.CardHeader("BLOC-testen", class_name="text-center fw-bold",
@@ -187,7 +195,6 @@ def init_callbacks(dash_app):
             ),
         ], class_name="mb-4")
 
-
     # This callback is used to dynamically return the statistics selection menu
     @dash_app.callback(
         Output('statistics_selection', 'children'),
@@ -225,7 +232,6 @@ def init_callbacks(dash_app):
             ),
         ], class_name="mb-4")
 
-
     # This callback is used to dynamically return the benchmark selection menu
     @dash_app.callback(
         Output('benchmark_selection', 'children'),
@@ -233,7 +239,7 @@ def init_callbacks(dash_app):
     def benchmark_selection(dashboard_data) -> dbc.Card:
         return dbc.Card([
             dbc.CardHeader("Benchmark", class_name="text-center fw-bold",
-                       style={"background-color": "#FF9900"}),
+                           style={"background-color": "#FF9900"}),
             dbc.CardBody(
                 [  # Benchmark selection dropdown
                     dcc.Dropdown(
@@ -242,9 +248,50 @@ def init_callbacks(dash_app):
                         id="bvo",
                         value=retrieve_filter_from_session("bvo"),
                     ), ],
-                ),
-            ])
+            ),
+        ])
 
+        # This callback is used to dynamically return the bloc test selection menu
+
+    @dash_app.callback(
+        Output('measurement_selection', 'children'),
+        [Input('filter_output', 'children')])
+    def measurement_selection(dashboard_data):
+        # if selected_dashboard == "algemene_motoriek":
+        #     return None
+
+        dashboard_data = pd.DataFrame(dashboard_data)
+        if dashboard_data.empty:
+            # PreventUpdate prevents ALL outputs updating
+            raise dash.exceptions.PreventUpdate
+
+        measurements = get_measurement_columns(dashboard_data)
+
+        session_statistics = retrieve_filter_from_session("measurement_selection")
+        print(measurement_selection)
+        if session_statistics is None:
+            session_statistics = measurements
+            save_filter_to_session("measurement_selection", measurements)
+        options = [{"label": f"{rename_column(entry)}", "value": f"{entry}"} for entry in measurements]
+
+        return dbc.Card([
+            dbc.CardHeader("Metingen", class_name="text-center fw-bold",
+                           style={"background-color": "#FF9900"}),
+            dbc.CardBody(
+                [dbc.Checklist(
+                    id="measurement_selection",
+                    options=options,
+                    value=measurements,
+                    label_checked_style={"color": "green"},
+                    input_style={"backgroundColor": "red"},
+                    input_checked_style={
+                        "backgroundColor": "green",
+                        "borderColor": "#green",
+                    },
+                ),
+                ],
+            ),
+        ], class_name="mb-4")
 
     @dash_app.callback(Output("filter_output", "children"),
                        [Input("dashboard_data", "children"),
@@ -253,8 +300,9 @@ def init_callbacks(dash_app):
                         Input("seizoen", "value"),
                         Input("bvo", "value"),
                         Input("bloc_test_selection", "value"),
+                        Input("measurement_selection", "value"),
                         Input("statistics", "value")])
-    def filter_data(dashboard_data, teams, lichting, seizoen, bvo, bloc_test_selection, statistics):
+    def filter_data(dashboard_data, teams, lichting, seizoen, bvo, bloc_test_selection, measurement_selection, statistics):
         dashboard_data = pd.DataFrame(dashboard_data)
         dashboard_data["geboortedatum"] = pd.to_datetime(dashboard_data["geboortedatum"])
 
@@ -274,11 +322,15 @@ def init_callbacks(dash_app):
         if bloc_test_selection is not None:
             dashboard_data = filter_bloc_tests(dashboard_data, bloc_test_selection)
 
+        if measurement_selection is not None:
+            dashboard_data = filter_measurements(dashboard_data, measurement_selection)
+
         save_filter_to_session("teams", teams)
         save_filter_to_session("lichting", lichting)
         save_filter_to_session("seizoen", seizoen)
         save_filter_to_session("bvo", bvo)
         save_filter_to_session("bloc_test_selection", bloc_test_selection),
+        save_filter_to_session("measurement_selection", measurement_selection),
         save_filter_to_session("statistics", statistics),
 
         return dashboard_data.to_dict(orient='records')
@@ -288,49 +340,69 @@ def init_callbacks(dash_app):
                        [Input("filter_output", "children")])
     # Input("statistics", "value"),])
     def create_line_chart(dashboard_data):
-        if dashboard_data is None:
+        dashboard_data = pd.DataFrame(dashboard_data)
+        if dashboard_data.empty:
             # PreventUpdate prevents ALL outputs updating
             raise dash.exceptions.PreventUpdate
 
-        bvo_id = session.get('id')
-        dashboard_data = pd.DataFrame(dashboard_data)
+        measurements = get_measurement_columns(dashboard_data)
+        result = calculate_mean_result_by_date(dashboard_data, measurements)
 
-        # #Get the columns that have the actual measurement
-        measurement_columns = list(set(dashboard_data.columns.values).symmetric_difference(META_COLUMNS))
-        measurement = sorted([col for col in measurement_columns if any(x in col for x in ['beste', 'totaal'])])
+        # if len(result['team_naam'].unique()) < 2:
+        #     print('not updating:', result['team_naam'].unique())
+        #     raise dash.exceptions.PreventUpdate
 
-        # This has got to go
-        mean = calculate_mean_result_by_date(dashboard_data, measurement)
-        club = calculate_mean_result_by_date(dashboard_data[dashboard_data['bvo_naam'] == bvo_id],
-                                             measurement)
+        bundled_df = [df for _, df in result.groupby('lichting')]
 
-        bundled_df: list[pd.DataFrame] = [club, mean]
+        line_dashes = ['solid', 'dot', 'longdash', 'dashdot', 'longdashdot']
 
-        fig = go.Figure()
-        for df in bundled_df:
-            x = df.index
-            for column in measurement:
-                y = df[column]
-                column_name = column.replace('_', ' ')
-                bvo_name = df['bvo_naam'].values[0]
-                name = f'{bvo_name} - {column_name}'
-                fig.add_trace(go.Scatter(x=x, y=y, name=name))
+        layout = go.Layout(autosize=True,
+                           height=625,
+                           margin=go.layout.Margin(l=50, r=50, b=100, t=100, pad=4)
+                           )
+        fig = go.Figure(layout=layout)
+        for idx, df_lichting in enumerate(bundled_df):
+            for jdx, measurement in enumerate(measurements):
+                x = df_lichting.index
+                y = df_lichting[measurement]
+                column_name = rename_column(measurement)
 
+                name = str(df_lichting['lichting'].values[0])
+                legendgroup = column_name
+                legendgrouptitle = dict(text=column_name)
+                mode = 'lines+markers'
+                # marker = dict(color=)
+                line = dict(color=get_colormap(idx), dash=line_dashes[jdx], width=3)
+                fig.add_trace(go.Scatter(
+                    x=x,
+                    y=y,
+                    name=name,
+                    legendgroup=legendgroup,
+                    legendgrouptitle=legendgrouptitle,
+                    mode=mode,
+                    # marker=marker,
+                    line_shape="spline",
+                    line=line,
+                ))
+
+        yaxis_title = 'Totaal score (punten)' if dashboard_data[
+                                                     measurements[0]].dtype == int else 'Beste of totaal resultaat'
         fig.update_layout(
-            yaxis_title='Totaal score (punten)',
-            xaxis_title='Datum',
-            legend_title="Teams",
+            xaxis=fix_labels(dashboard_data),
+            yaxis_title=yaxis_title,
+            xaxis_title='Meet moment',
+            legend_title="Lichtingen",
             title_x=0.5
         )
-        fig = add_figure_rangeslider(fig)
+        # add_figure_rangeslider(fig)
         return fig
 
-    # TODO: ################### INFO CARD ####################
-    @dash_app.callback(Output("overview", "children"),
-                       [Input("line_chart", "selectedData"),
-                        Input("line_chart", "relayoutData")])
-    def update_line_chart_info_card(selectedData, relayoutData):
-        return f'{selectedData}, {relayoutData}'
+    # ################### INFO CARD ####################
+    # @dash_app.callback(Output("overview", "children"),
+    #                    [Input("line_chart", "selectedData"),
+    #                     Input("line_chart", "relayoutData")])
+    # def update_line_chart_info_card(selectedData, relayoutData):
+    #     return f'{selectedData}, {relayoutData}'
 
     # This callback is used to dynamically return the bloc test chart
     @dash_app.callback(
@@ -355,7 +427,6 @@ def init_callbacks(dash_app):
     def show_boxplot(statistics):
         if statistics is not None and "boxplot" in statistics:
             return dcc.Graph(id="boxplot", responsive=True)
-
 
     # This callback is used to dynamically create a boxplot
     @dash_app.callback(
