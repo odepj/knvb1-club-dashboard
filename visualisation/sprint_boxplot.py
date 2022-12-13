40897d9b435d0f48a244f6d5c90e9e3974a84ec4
Sandro Boelsz <sandro.boelsz@hva.nl>
2022-12-13 22:27:16 +0100

    Extra changes for median and average values

--- visualisation/sprint_boxplot.py
+++ visualisation/sprint_boxplot.py
@@ -4,11 +4,8 @@ from visualisation.dashboard_template_functions import get_measurement_columns, 
 
 
 def create_boxplot_function(individuen, dataframe: pd.DataFrame) -> go.box:
-    dataframe.sort_values(['team_naam'], inplace=True)
+    extra_columns = dataframe.filter(regex='.mediaan|.gemiddelde').columns.values
     selected_columns = get_measurement_columns(dataframe)
-
-    df_median = dataframe.groupby("team_naam").median(numeric_only=True).reset_index()
-    df_median.sort_values(['team_naam'], inplace=True)
 
     fig = go.Figure()
     for column in selected_columns:
@@ -19,18 +16,21 @@ def create_boxplot_function(individuen, dataframe: pd.DataFrame) -> go.box:
             y=dataframe[column],
             name=column_name,
             boxpoints=individuen,
-            line=dict()))
-
-        fig.add_trace(go.Scatter(
-        x=df_median['team_naam'],
-        y=df_median[column],
-        name=f"{column_name} mediaan",
-        mode="markers",
-        marker=dict(size=80, symbol="line-ew", line=dict(width=2, color="red"))
+            line=dict()
     ))
 
-    fig.update_layout(title_text="Sprint", autosize=False, width=1400, height=2600)
-    fig.update_layout(yaxis_title="sprint scores")
+    for extra_column in extra_columns:
+        column_name = rename_column(extra_column)
+
+        fig.add_trace(go.Scatter(
+        x=dataframe['team_naam'],
+        y=dataframe[extra_column],
+        name=column_name,
+        mode="markers",
+        marker=dict(size=80, symbol="line-ew", line=dict(width=2, color="red"))))
+    
+    fig.update_layout(title_text="<b>Sprint vergelijking van teams<b>", autosize=False, width=1400, height=2600)
+    fig.update_layout(yaxis_title="sprint score")
     fig.update_layout(xaxis_title="team naam")
 
     return fig
