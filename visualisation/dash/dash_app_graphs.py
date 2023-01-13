38b0d3d4eb38f3f6524f0a4f6a85c731f5844c80
Sandro Boelsz <sandro.boelsz@hva.nl>
2023-01-13 13:40:42 +0100

    added boxplot by lichtingen chart on filter option, removed boxplot remove option

--- visualisation/dash/dash_app_graphs.py
+++ visualisation/dash/dash_app_graphs.py
@@ -1,14 +1,15 @@
 import plotly.graph_objects as go
-
+from plotly.subplots import make_subplots
 
 from visualisation.dash.dash_app_functions import *
 
 
-def create_box(individuen, dataframe: pd.DataFrame) -> go.box:
+def create_boxplot(individuen, dataframe: pd.DataFrame) -> go.box:
     dataframe.sort_values(['team_naam'], inplace=True)
     extra_columns = dataframe.filter(regex='.mediaan|.gemiddelde').columns.values
     selected_columns = dataframe[get_measurement_columns(dataframe)]
     selected_columns = drop_mean_and_median_columns(selected_columns)
+    measurements = selected_columns.columns.values.tolist()
 
     fig = go.Figure()
     for column in selected_columns:
@@ -19,47 +20,12 @@ def create_box(individuen, dataframe: pd.DataFrame) -> go.box:
             y=dataframe[column],
             name=column_name,
             boxpoints=individuen,
-            line=dict(),
+            line=dict(color=get_colormap(measurements.index(column))),
             showlegend=True
         ))
 
     for extra_column in extra_columns:
         column_name = rename_column(extra_column)
-        fig.add_trace(go.Scatter(
-            x=dataframe['team_naam'],
-            y=dataframe[extra_column],
-            name=column_name,
-            mode="markers",
-            showlegend=True,
-            marker=dict(size=80, symbol="line-ew", line=dict(width=2, color="red"))))
-
-    fig.update_layout(title_text="<b>Vergelijking van teams<b>", autosize=True)
-    fig.update_layout(yaxis_title="Beste van totaal resultaat")
-    fig.update_layout(xaxis_title="team naam")
-
-    return fig
-
-
-def create_boxplot_function(individuen, dataframe: pd.DataFrame) -> go.box:
-    extra_columns = dataframe.filter(regex='.mediaan|.gemiddelde').columns.values
-    selected_columns = get_measurement_columns(dataframe)
-
-    fig = go.Figure()
-    for column in selected_columns:
-        column_name = rename_column(column)
-
-        fig.add_trace(go.Box(
-            x=dataframe['team_naam'],
-            y=dataframe[column],
-            name=column_name,
-            boxpoints=individuen,
-            line=dict(),
-            showlegend=True
-        ))
-
-    for extra_column in extra_columns:
-        column_name = rename_column(extra_column)
-
         fig.add_trace(go.Scatter(
             x=dataframe['team_naam'],
             y=dataframe[extra_column],
@@ -68,10 +34,22 @@ def create_boxplot_function(individuen, dataframe: pd.DataFrame) -> go.box:
             showlegend=True,
             marker=dict(size=80, symbol="line-ew", line=dict(width=2, color="red")))),
 
-    fig.update_layout(title_text="<b>Sprint vergelijking van teams<b>", autosize=True, width=1400, height=2600)
+    return fig
+
+
+def create_box(individuen, dataframe: pd.DataFrame) -> go.Box:
+    fig = create_boxplot(individuen, dataframe)
+    fig.update_layout(title_text="<b>Vergelijking van teams<b>", autosize=True)
+    fig.update_layout(yaxis_title="Beste van totaal resultaat")
+    fig.update_layout(xaxis_title="team naam")
+    return fig
+
+
+def create_boxplot_function(individuen, dataframe: pd.DataFrame) -> go.Box:
+    fig = create_boxplot(individuen, dataframe)
+    fig.update_layout(title_text="<b>Sprint vergelijking van teams<b>", autosize=True)
     fig.update_layout(yaxis_title="sprint score")
     fig.update_layout(xaxis_title="team naam")
-
     return fig
 
 
@@ -85,7 +63,8 @@ def create_line(filter_output, dashboard_data, statistics):
     df_mean_median = [(calculate_result_by_date(dashboard_data, measurements, statistic), statistic) for statistic
                       in additional_traces]
 
-    fig = go.Figure(layout=go.Layout(autosize=True, height=625))
+    line = go.Figure(layout=go.Layout(autosize=True))
+    box = go.Figure(layout=go.Layout(autosize=True))
 
     # Generate the traces for statistics
     dash_dict = {'gemiddelde': 'solid', 'mediaan': 'dot'}
@@ -95,7 +74,7 @@ def create_line(filter_output, dashboard_data, statistics):
         df = df[df.index.isin(index)]
         for jdx, measurement in enumerate(measurements):
             column_name = rename_column(measurement)
-            fig.add_trace(go.Scatter(
+            line.add_trace(go.Scatter(
                 x=df.index,
                 y=df[measurement],
                 name=statistic,
@@ -112,7 +91,7 @@ def create_line(filter_output, dashboard_data, statistics):
         name = str(df_lichting['lichting'].values[0])
         for jdx, measurement in enumerate(measurements):
             column_name = rename_column(measurement)
-            fig.add_trace(go.Scatter(
+            line.add_trace(go.Scatter(
                 x=df_lichting.index,
                 y=df_lichting[measurement],
                 name=name,
@@ -122,21 +101,50 @@ def create_line(filter_output, dashboard_data, statistics):
                 line_shape="spline",
                 line=dict(color=get_colormap(idx), dash=dash_list[jdx], width=3),
             ))
+                
+            if "boxplot" in statistics:
+                box.add_trace(go.Box(
+                    x=df_lichting["lichting"],
+                    y=df_lichting[measurement],
+                    name=column_name,
+                    showlegend=True,
+                    legendgroup=column_name,
+                    line=dict(color=get_colormap(measurements.index(measurement))))
+                )
+
+                # make sure that there is only one of the names in the legend
+                names = set()
+                box.for_each_trace(
+                    lambda trace: trace.update(showlegend=False) 
+                    if (trace.name in names) else names.add(trace.name))
+
 
     if len(measurements) > 0:
         predicate = filter_output[measurements[0]].dtype == int
-        yaxis_title = 'Totaal score (punten)' if predicate else 'Beste of totaal resultaat'
+        yaxis_title = 'Totaal score (punten)' if predicate else 'Beste/totaal resultaat'
     else:
         yaxis_title = 'Geen selectie'
 
-    fig.update_layout(
+    line.update_layout(
         xaxis=fix_labels(filter_output),
         yaxis_title=yaxis_title,
         xaxis_title='Meet moment',
         title_text="<b>Ontwikkeling lichtingen<b>"
     )
-    return fig
 
+    if "boxplot" in statistics:
+        fig = make_subplots(rows=2, cols=1, y_title=yaxis_title)
+        fig.add_traces(line.data, rows=1, cols=1)
+        fig.add_traces(box.data, rows=2, cols=1)
+        fig.update_layout(title_text="<b>Ontwikkeling lichtingen<b>")
+
+        # edit axis labels
+        fig['layout']['xaxis']['title']='Meet moment'
+        fig['layout']['xaxis2']['title']='Lichting'
+
+        return fig
+    
+    return line
 
 # This dictionary will be used to lookup BLOC-test specific rows
 columns = {"Evenwichtsbalk": ["Balance_Beam_3cm", "Balance_Beam_4_5cm", "Balance_Beam_6cm", "Balance_beam_totaal"],
