e1105fe69d31833a4ad7780c4784a9d16e037c99
Sandro Boelsz <sandro.boelsz@hva.nl>
2023-01-27 15:39:51 +0100

    Changed the way environment variables are being read for ease of use with Docker

--- database/__init__.py
+++ database/__init__.py
@@ -10,18 +10,33 @@ from dotenv import load_dotenv
 # load in the environment variables
 load_dotenv()
 
-# MySQL Server connection configuration
-connect_args = {'ssl': {'fake_flag_to_enable_tls': True}}
-connect_string = 'mysql+pymysql://{}:{}@{}/{}'.format(os.getenv('MYSQL_USER'), os.getenv(
-   'MYSQL_PASSWORD'), os.getenv('MYSQL_HOST'), os.getenv('MYSQL_DB'))
-engine = create_engine(connect_string, connect_args=connect_args, echo=False)
+AZURE_DB = os.getenv('AZURE_DB')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DB = os.getenv('MYSQL_DB')
 
-# Azure Server connection configuration
-#params = urllib.parse.quote_plus(os.getenv('AZURE_DB'))
-#connect_string = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-#engine = create_engine(connect_string, echo=True)
+# If the AZURE_DB environment variable is not None
+# then we know that we are dealing with an Azure Database
+if AZURE_DB is not None:
+    # Azure Server connection configuration
+    params = urllib.parse.quote_plus(os.getenv('AZURE_DB'))
+    connect_string = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+    engine = create_engine(connect_string, echo=True)
 
-# Echo=True enables logging
+# Same can be said for MySQL, if the MYSQL env variables are not None or an empty string
+# then we know that we are dealing with a MySQL Database
+elif None not in (MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DB):
+    # MySQL Server connection configuration
+    connect_args = {'ssl': {'fake_flag_to_enable_tls': True}}
+    connect_string = 'mysql+pymysql://{}:{}@{}/{}'.format(os.getenv('MYSQL_USER'), os.getenv(
+       'MYSQL_PASSWORD'), os.getenv('MYSQL_HOST'), os.getenv('MYSQL_DB'))
+    engine = create_engine(connect_string, connect_args=connect_args, echo=False)
+else:
+    print("No suitable database connection variables were found!")
+    
+
+# Echo=True enables loggingø
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
