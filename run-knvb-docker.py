cb90596285350acfda0e34a0544c87badb2331bd
Sandro Boelsz <sandro.boelsz@hva.nl>
2023-01-28 12:58:08 +0100

    Changed startup script for testing the docker containers

--- run-knvb-docker.py
+++ run-knvb-docker.py
@@ -1,9 +1,8 @@
 import os
 
-IMAGE_NAME = "knvb-team-2"
-
 
 def handle_mysql_input() -> dict:
+    image_name = input("Enter the IMAGE_NAME: ")
     container_name = input("Enter the CONTAINER_NAME: ")
     port = input("Enter the PORT: ")
     mysql_user = input("Enter the MYSQL_USER: ")
@@ -11,18 +10,19 @@ def handle_mysql_input() -> dict:
     mysql_host = input("Enter the MYSQL_HOST: ")
     mysql_db = input("Enter the MYSQL_DB: ")
 
-    return {"container_name": container_name, "port": port,
-            "mysql_user": mysql_user, "mysql_password": mysql_password,
+    return {"image_name": image_name, "container_name": container_name,
+            "port": port, "mysql_user": mysql_user, "mysql_password": mysql_password,
             "mysql_host": mysql_host, "mysql_db": mysql_db}
 
 
 def handle_mssql_input() -> dict:
+    image_name = input("Enter the IMAGE_NAME: ")
     container_name = input("Enter the CONTAINER_NAME: ")
     port = input("Enter the PORT: ")
     azure_db = input("Enter the AZURE_DB: ")
 
-    return {"container_name": container_name, "port": port,
-            "azure_db": azure_db}
+    return {"image_name": image_name, "container_name": container_name,
+            "port": port, "azure_db": azure_db}
 
 
 def generate__mysql_command(input: dict) -> str:
@@ -32,28 +32,34 @@ def generate__mysql_command(input: dict) -> str:
         -e MYSQL_HOST={input['mysql_host']} \
         -e MYSQL_DB={input['mysql_db']} \
         --name {input['container_name']} \
-        {IMAGE_NAME}"
+        {input['image_name']}"
 
 
 def generate__mssql_command(input: dict) -> str:
     return f"docker run -p {input['port']}:5000 \
         -e AZURE_DB={input['azure_db']} \
         --name {input['container_name']} \
-        {IMAGE_NAME}"
+        {input['image_name']}"
 
 
 def mysql() -> None:
     input = handle_mysql_input()
-    os.system(generate__mysql_command(input)) 
+    os.system(generate__mysql_command(input))
 
 
 def mssql() -> None:
     input = handle_mssql_input()
-    os.system(generate__mssql_command(input)) 
+    os.system(generate__mssql_command(input))
 
 
 def main() -> None:
-    mysql()
+    type = input("MYSQL(1) or MSSQL(2):  ")
+
+    if type == "1" or type == "MYSQL":
+        mysql()
+    elif type == "2" or type == "MSSQL":
+        mssql()
+    return
 
 
 if __name__ == '__main__':
